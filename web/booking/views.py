1c489c57960473f81b4e4d6fa444707aeb012bc1
Konstantin <wetarmor@gmail.com>
2019-03-23 16:00:21 +0300

    Added Buses, Clients and Bindings viewsets

--- web/booking/views.py
+++ web/booking/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from . import models
+from . import serializers
+
+
+class BusesViewSet(viewsets.ModelViewSet):
+    queryset = models.Buses.objects.all()
+    serializer_class = serializers.BusesSerializer
+
+
+class ClientsViewSet(viewsets.ModelViewSet):
+    queryset = models.Clients.objects.all()
+    serializer_class = serializers.ClientsSerializer
+
+
+class BindingsViewSet(viewsets.ModelViewSet):
+    queryset = models.Bindings.objects.all()
+    serializer_class = serializers.BindingsSerializer
