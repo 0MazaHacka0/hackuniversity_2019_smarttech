1006ec6bd3f3ac727b9ced9853a8dd9f979cba92
Konstantin <wetarmor@gmail.com>
2019-03-23 15:58:56 +0300

    Added Buses, Clients and Bindings models

--- web/booking/models.py
+++ web/booking/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Buses(models.Model):
+    bus_id = models.UUIDField(unique=True, blank=False)
+    gosnum = models.CharField(max_length=9)
+
+
+class Clients(models.Model):
+    client_id = models.UUIDField(unique=True, blank=False)
+    disability_card = models.CharField(max_length=60)
+    approved = models.BooleanField()
+
+
+class Bindings(models.Model):
+    bus_id = models.ForeignKey(Buses, on_delete=models.CASCADE)
+    client_id = models.ForeignKey(Clients, on_delete=models.CASCADE)
+    start_point = models.TextField()
+    end_point = models.TextField()
