4d4e9e20ed19cd19789e74b7d46b6aa6b61a0a86
0MazaHacka0 <killerinshadow2@gmail.com>
2019-03-24 12:06:24 +0300

    APP

---
+++ android-app/legacy/SocialStep/app/src/main/java/com/smarttech/socialstep/ui/activities/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.smarttech.socialstep.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.smarttech.socialstep.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+}
